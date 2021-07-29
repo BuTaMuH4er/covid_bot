24bb2feb47b8115be9575f1b710457af9af99faa
Alexander Kozhokhin <kozhokhin@gmail.com>
2021-07-29 16:33:05 +0300

    Добавил класс для очереди на отправку сообщений

--- covidla_bot.py
+++ covidla_bot.py
@@ -1,17 +1,47 @@
 from config import API_KEY_BOT
 from telegram import ReplyKeyboardMarkup, ParseMode
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
-import csv, logging, time
+import csv, logging, telegram.bot
 from datetime import datetime
 from take_vacine import take_data_message, write_or_not, take_vaccine_db, add_user_db, remove_user_db, distribution_list, del_distribution_list
 from celery import Celery
 from celery.schedules import crontab
+from telegram.ext import  messagequeue as mq
+from telegram.utils.request import Request
 
 celery_app = Celery('covid_tasks', broker='redis://localhost:6379/0')
-mybot = Updater(API_KEY_BOT, use_context=True)
-dp = mybot.dispatcher
-
 logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+class MQBot(telegram.bot.Bot):
+    def __init__(self, *args, is_queued_def=True, msq_queue=None, **kwargs):
+        super(MQBot, self).__init__(*args, **kwargs)
+        self._is_messages_queued_default = is_queued_def
+        self._msg_queue = msq_queue or mq.MessageQueue()
+
+    def __del__(self):
+        try:
+            self._msg_queue.stop()
+        except:
+            pass
+
+    @mq.queuedmessage
+    def send_message(self, *args, **kwargs):
+        return super(MQBot, self).send_message(*args, **kwargs)
+
+
+def main():
+    request = Request( con_pool_size=8)
+    bot = MQBot(API_KEY_BOT, request=request)
+    mybot = Updater(bot=bot, use_context=True)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', start_bot, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('^Да$'), add_user))
+    dp.add_handler(MessageHandler(Filters.regex('^Нет$'), remove_user))
+    dp.add_handler(MessageHandler(Filters.regex('^Есть чо'), send_data_vaccine))
+    time_now = datetime.today().strftime("%H:%M:%S  %d/%m/%Y")
+    logging.info(f'{time_now} Бот стартовал')
+    mybot.start_polling()
+    mybot.idle()
 
 
 def read_list_users():
@@ -78,24 +108,12 @@ def send_new_data_vaccine(context):
         for men in chat_ids:
             for key, val in vaccines.items():
                 dp.bot.send_message(chat_id=men, text=f'<b>{key}</b>:\n{val}', parse_mode=ParseMode.HTML)
-                time.sleep(1)
             del_distribution_list(men)
-            time.sleep(15) #пока сообщения будут так отправляться, необходимо написать класс для очереди сообщений
 
 
 @celery_app.on_after_configure.connect
 def periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(minute='*/10'), send_new_data_vaccine.s())
-    #sender.add_periodic_task(600.0, send_new_data_vaccine.s())
 
 if __name__ == '__main__':
-
-
-    dp.add_handler(CommandHandler('start', start_bot, pass_user_data=True))
-    dp.add_handler(MessageHandler(Filters.regex('^Да$'), add_user))
-    dp.add_handler(MessageHandler(Filters.regex('^Нет$'), remove_user))
-    dp.add_handler(MessageHandler(Filters.regex('^Есть чо'), send_data_vaccine))
-    time_now = datetime.today().strftime("%H:%M:%S  %d/%m/%Y")
-    logging.info(f'{time_now} Бот стартовал')
-    mybot.start_polling()
-    mybot.idle()
+    main()
